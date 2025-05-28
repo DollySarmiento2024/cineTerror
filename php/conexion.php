d0244120862150dc7b19b7c9f86188b06262817d
DollySarmiento2024 <dosaqu@alumnatflorida.es>
2025-05-28 19:07:19 +0200

    Primer commit

---
+++ php/conexion.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "db";
+$username = "fernanda";
+$password = "fernanda";
+$dbname = "cine";
+// Crear la conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+?>
